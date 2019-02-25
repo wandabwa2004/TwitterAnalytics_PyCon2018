12f0c5e3603907cf0d6a5bfd4fd04229ead137d5
Ryan Nazareth <ryankarlos@gmail.com>
2019-02-25 00:08:31 +0000

    Add local runner and modularise preprocessing and word2vec code

---
+++ local runner/exploring_tweets.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from exploration.word2vec_tsne_LDA import word_count, word2vec_model, \
+    bag_of_words_corpus, gensim_dict, tfidf_corpus, tsne_model_fit, \
+    plotly_tsne, plot_tsne, lda_model
+
+
+perplexity = [10, 50, 75, 100]
+iterations = [1000, 5000]
+no_below = 15
+no_above = 0.5
+keep_n = 100000
+top = 10
+
+
+tweet_df = pd.read_csv('snakebite.csv', sep=',', encoding='latin-1')
+top_words = word_count(tweet_df, top)
+print(top_words)
+
+keyword_list = ['snakebite', 'snake', 'anti', 'venom', 'n', 'bite']
+sentence_dictionary = gensim_dict(tweet_df, keyword_list)
+sentence_list = sentence_dictionary['sentence_list']
+dictionary = sentence_dictionary['dictionary']
+
+bow_corpus = bag_of_words_corpus(sentence_list,
+                                 dictionary, no_below, no_above, keep_n)
+corpus_tfidf = tfidf_corpus(bow_corpus)
+lda_topics = lda_model(corpus_tfidf, dictionary)
+word2vec_corpus = word2vec_model(sentence_list)
+vocab = word2vec_corpus['vocab']
+X_transformed = word2vec_corpus['X_transformed']
+
+df = tsne_model_fit(X_transformed, vocab, perplexity, iterations)
+plot_tsne(df['x'], df['y'], df['word'])
+plotly_tsne(df)
--- preprocessing/twitter_texttprocessing.py
+++ preprocessing/twitter_texttprocessing.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
-from TwitterSearch import *
+import TwitterSearch
+from TwitterSearch import TwitterSearchException, TwitterSearchOrder
 from dateutil.parser import parse
 import os
 import re
@@ -29,13 +30,13 @@ ts = TwitterSearch(
     tweet_mode='extended')
 
 keyword_list = Config.get('Keywords', 'keyword_list')
-keywords_filter= Config.get('Keywords', 'keywords_filter')
+keywords_filter = Config.get('Keywords', 'keywords_filter')
 keyword_combo = Config.get('Keywords', 'keyword_combo')
 stop = Config.get('Stopwords', 'stop')
 filter_users = Config.get('Usernames', 'filter_users')
 
 
-def tweet_streaming(keywords= keyword_combo):
+def tweet_streaming(keywords=keyword_combo):
     tweet_list = []
 
     for word in keywords:
@@ -43,19 +44,28 @@ def tweet_streaming(keywords= keyword_combo):
             tso.set_keywords(word, or_operator=False)
 
             for tweet in ts.search_tweets_iterable(tso):
-                print('%s@%s tweeted: %s' % (tweet['created_at'], tweet['user']['screen_name'], tweet['text']))
+                print('%s@%s tweeted: %s' %
+                      (tweet['created_at'],
+                       tweet['user']['screen_name'],
+                       tweet['text']))
                 print()
-                tweet_list.append([tweet['id'], tweet['created_at'], tweet['user']['screen_name'],
-                                   tweet['favorite_count'], tweet['retweet_count'], tweet['user']['followers_count'],
-                                   tweet['user']['friends_count'], tweet['text'], tweet['user']['time_zone'],
+                tweet_list.append([tweet['id'],
+                                   tweet['created_at'],
+                                   tweet['user']['screen_name'],
+                                   tweet['favorite_count'],
+                                   tweet['retweet_count'],
+                                   tweet['user']['followers_count'],
+                                   tweet['user']['friends_count'],
+                                   tweet['text'],
+                                   tweet['user']['time_zone'],
                                    tweet['user']['location']])
 
         except TwitterSearchException as e:
             print('There was a problem searching for the Tweets')
 
     tweet_frame = pd.DataFrame(tweet_list, columns=(
-        'User_id', 'Date', 'User', 'Favourite_count', 'Retweet_count', 'Followers_count', 'Friends_count', 'Text',
-        'Time_zone',
+        'User_id', 'Date', 'User', 'Favourite_count', 'Retweet_count',
+        'Followers_count', 'Friends_count', 'Text', 'Time_zone',
         'Location'))
 
     tweet_frame = tweet_frame.dropna(subset=['Text'])
@@ -67,13 +77,17 @@ def tweet_streaming(keywords= keyword_combo):
 def removeusers(tweet_frame):
     row = list()
     for i in range(0, tweet_frame.shape[0]):
-        if any(word in tweet_frame['User'][i] for word in filter_users):
+        if any(word in tweet_frame['User'][i]
+               for word in filter_users):
             row.append(i)
-        elif filter((lambda x: re.search('game', x)), tweet_frame['User'][i]) == ('game'):
+        elif filter((lambda x: re.search('game', x)),
+                    tweet_frame['User'][i]) == ('game'):
             row.append(i)
-        elif filter((lambda x: re.search('snakebite', x)), tweet_frame['User'][i]) == ('snakebite'):
+        elif filter((lambda x: re.search('snakebite', x)),
+                    tweet_frame['User'][i]) == ('snakebite'):
             row.append(i)
-        elif any(word in tweet_frame['Text'][i] for word in filter_users):
+        elif any(word in tweet_frame['Text'][i]
+                 for word in filter_users):
             row.append(i)
 
     tweet_frame.drop(tweet_frame.index[row], inplace=True)
@@ -93,7 +107,8 @@ def remove_emoji(tweet, emoji_pattern):
 def date_timestamp(tweet):
     tweet['Timestamp'] = tweet.apply(lambda r: r.Date[11:20], axis=1)
     tweet['Date'] = tweet.apply(lambda r: r.Date[4:11], axis=1)
-    tweet['Date'] = tweet.apply(lambda r: parse(r.Date).strftime('%d/%m/%Y'), axis=1)
+    tweet['Date'] = tweet.apply(lambda r: parse(r.Date).strftime('%d/%m/%Y'),
+                                axis=1)
 
     return tweet
 
@@ -109,9 +124,11 @@ def cleantweet(tweet, lemmatiser, stemmer=None):
         str_no_username = re.sub(r'(?:@[\w_]+)', '', str_letters_only)
         str_no_username = str_no_username.strip()
         exclude = set(string.punctuation)
-        str_no_punc = "".join(word for word in str_no_username if word not in exclude)
+        str_no_punc = "".join(word for word in str_no_username
+                              if word not in exclude)
         tokenised = nltk.word_tokenize(str_no_punc)
-        stop_free = [word for word in tokenised if word not in stopwords.words('english')]
+        stop_free = [word for word in tokenised
+                     if word not in stopwords.words('english')]
         word_lemm = [lemmatiser.lemmatize(t) for t in stop_free]
         if stemmer is not None:
             word_stem = [stemmer.stem(i) for i in word_lemm]
@@ -144,10 +161,10 @@ if __name__ == "__main__":
     raw_tweets = tweet_streaming()
     filter_users = removeusers(raw_tweets)
     emoji_pattern = re.compile("["
-                               u"\U0001F600-\U0001F64F"  # emoticons
-                               u"\U0001F300-\U0001F5FF"  # symbols & pictographs
-                               u"\U0001F680-\U0001F6FF"  # transport & map symbols
-                               u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                               u"\U0001F600-\U0001F64F"
+                               u"\U0001F300-\U0001F5FF"
+                               u"\U0001F680-\U0001F6FF"
+                               u"\U0001F1E0-\U0001F1FF"
                                "]+", flags=re.UNICODE)
 
     filtered_emojis = remove_emoji(filter_users, emoji_pattern)
@@ -159,4 +176,5 @@ if __name__ == "__main__":
     path = os.getcwd() + '/data/snakebite.csv'
 
     with open(path, 'w') as f:
-        cleaned_tweets.to_csv('snakebite1.csv', index=False, header=False, mode='a')
+        cleaned_tweets.to_csv('snakebite1.csv',
+                              index=False, header=False, mode='a')
